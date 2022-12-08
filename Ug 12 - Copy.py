3d9dab1b8096ce9ce3fc88443180c74237d09b52
Alvionaputri07 <117884597+Alvionaputri07@users.noreply.github.com>
2022-12-08 12:55:45 +0700

    Add files via upload

---
+++ Ug 12 - Copy.py
@@ -0,0 +1,6 @@
+# print ("SELAMAT DATANG DI PROGRAM PEMBUAT PIRAMIDA BERLUBANG")
+# o = int(input("Masukkan angka: "))
+# print(' '*(o-1),"*")
+# for p in range ((o-2),0,-1):
+#     print(' '*(p-1),"**")
+# print ("**"*o)
