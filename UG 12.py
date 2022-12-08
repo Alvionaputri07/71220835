47bb720fe9b90e9ffd878b5ac1614103fbad943e
Alvionaputri07 <117884597+Alvionaputri07@users.noreply.github.com>
2022-12-08 13:04:48 +0700

    Add files via upload

---
+++ UG 12.py
@@ -0,0 +1,5 @@
+# print ("~~~ Selamat Datang di Kalkulator Sederhana ~~~")
+# print = (int(input("Masukkan operator matematika yang ingin Anda hitung: ")))
+# print = (int(input("Mau penjumlahan berapa: ")))
+# print = (int(input("Print berapa banyak:" )))
+# c=1
