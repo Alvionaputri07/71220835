d4f8ec9bf44ae38d4c1f78a3a3379a3fb5f26db2
Alvionaputri07 <117884597+Alvionaputri07@users.noreply.github.com>
2022-12-15 12:51:47 +0700

    Add files via upload

---
+++ Ug 13.py
@@ -0,0 +1,36 @@
+import turtle
+s = turtle.Screen()
+t = turtle.Turtle()
+t.color('purple')
+t.penup()
+t.goto(-300, 60)
+t.pendown()
+t.left(65)
+t.forward(100)
+t.pendown()
+t.right(130)
+t.forward(100)
+t.right(180)
+t.forward(50)
+t.left(65)
+t.forward(45)
+t.penup()
+t.right(180)
+t.forward (110)
+t.pendown()
+t.circle (20.360)
+# t.forward()
+# t.forward(180)
+t.pendown
+t.circle(-30.360)
+# t.forward()
+t.penup()
+t.down(100)
+t.forward(100)
+t.right (100)
+
+
+
+s.exitonclick()
+
+
